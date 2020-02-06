a8965570f14c3945dd27a7512731daa094590306
William Lin <willlin1996@gmail.com>
2020-02-06 17:44:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,6 +12,6 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split
   sentence_array.collect {|word| word.reverse!}
-  sentence_array.join(' ')
-  return sentence_array
+  new_sentence_array = sentence_array.join(' ')
+  return new_sentence_array
 end
