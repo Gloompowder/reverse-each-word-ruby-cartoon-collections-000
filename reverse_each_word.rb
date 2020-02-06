2db2a1a67ea503b40f8711ffa7146801307c2fe9
William Lin <willlin1996@gmail.com>
2020-02-06 17:45:07 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+# def reverse_each_word(sentence)
+#   sentence_array = sentence.split
+#   sentence_array.each.collect do |word|
+#     word.reverse!
+# end
+# # new_sentence = sentence_array.join(' ')
+# new_sentence_array = sentence_array.join(' ')
+# # return new_sentence
+# return new_sentence_array
+# end
+
+def reverse_each_word(sentence)
+  sentence_array = sentence.split
+  sentence_array.collect {|word| word.reverse!}
+  new_sentence_array = sentence_array.join(' ')
+  return new_sentence_array
+end
